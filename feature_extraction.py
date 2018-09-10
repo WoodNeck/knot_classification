0923a85c957778dfe2f5ac5956e61d6a05d47576
WoodNeck <saiyu915@naver.com>
2018-09-10 11:48:59 +0900

    feat: Summation 방법으로 SIFT descriptor reduce

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,68 +4,45 @@ import h5py
 from sklearn.cluster import KMeans
 from scipy.cluster.vq import vq
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
+
+DESCRIPTOR_LENGTH = 128  # 4 * 4 * 8(논문 기준)
 
 
 def extract_feature(img_paths, img_labels):
     total_descriptors = np.array([])
-    descriptor_lengths = []
-    train_labels = []
 
-    total = len(img_paths)
-    processed = 0
-    for i in range(total):
-        img_path = img_paths[i]
-        label = img_labels[i]
-        train_labels.append(label)
-
+    total = len(img_paths)  # 총 이미지 개수
+    processed = 0  # 현재 처리한 이미지 개수
+    for img_path in img_paths:
         img = cv2.imread(img_path)
-
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        sift = cv2.xfeatures2d.SIFT_create()
 
+        sift = cv2.xfeatures2d.SIFT_create()
         (keypoints, descriptors) = sift.detectAndCompute(gray, None)
+
         if descriptors is not None:
             descriptors = descriptors.astype(np.float32, copy=False)
-            total_descriptors = np.append(total_descriptors, descriptors)
-            descriptor_lengths.append(descriptors.shape[0])
+            descriptors = descriptors.reshape(-1, DESCRIPTOR_LENGTH)
+            descriptor_summed = np.add.reduce(descriptors)
+            total_descriptors = np.append(total_descriptors, descriptor_summed)
         else:
-            descriptor_lengths.append(0)
+            zero_descriptor = np.zeros(DESCRIPTOR_LENGTH, "float32")
+            total_descriptors = np.append(total_descriptors, zero_descriptor)
+
         processed += 1
-        if processed % 20 == 0:
+        if processed % 100 == 0:
             print("[SIFT]{}/{}".format(processed, total))
 
-    total_descriptors = total_descriptors.reshape(-1, 128)
+    total_descriptors = total_descriptors.reshape(-1, DESCRIPTOR_LENGTH)
     print(total_descriptors.shape)
 
-    # Perform k-means clustering
-    print("Starting k-means")
-    k = 500 # 데이터의 개수에 비례하게 설정
-    kmeans = KMeans(n_clusters=k, verbose=1).fit(total_descriptors)
-    print("k-means clustering done...")
-
-    # Calculate the histogram of features
-    index = 0
-    feature_vectors = np.array([])
-    for desc_length in descriptor_lengths:
-        predicted = np.zeros(k, "float32")
-        for i in range(desc_length):
-            descriptor = total_descriptors[index]
-            descriptor = descriptor.reshape(1, -1)
-            predicted[kmeans.predict(descriptor)[0]] += 1
-            index += 1
-        feature_vectors = np.append(feature_vectors, predicted)
-    feature_vectors = feature_vectors.reshape(-1, k)
-    print(feature_vectors.shape)
-
+    # 라벨을 String에서 Integer로 변환
     label_encoder = LabelEncoder()
-    target = label_encoder.fit_transform(train_labels)
+    target = label_encoder.fit_transform(img_labels)
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    rescaled_features = scaler.fit_transform(feature_vectors)
-
+    # Feature Extracted된 저장
     h5f_data = h5py.File('data/data.h5', 'w')
-    h5f_data.create_dataset('dataset_1', data=np.array(rescaled_features))
+    h5f_data.create_dataset('dataset_1', data=total_descriptors)
 
     h5f_label = h5py.File('data/labels.h5', 'w')
     h5f_label.create_dataset('dataset_1', data=np.array(target))
--- todo/TODO.md
+++
@@ -1,8 +0,0 @@
-기존 모델
-- SIFT로 추출된 모든 descriptors -> k-Means를 통해서 실제로 어떤 타입의 descriptor들이 어떠한 빈도를 갖고 분포하는지를 **가공된** 특징 벡터로써 추출 -> kNN을 통해서 가공된 특징 벡터를 이용하여 이미지 분류 수행
-
-
-새로운 모델
-SIFT 단계는 동일하게 구성 -> 하나의 이미지에서 발생하는 모든 descriptor에 대해 128size의 하나의 descriptor로 reduce -> 이를 kNN의 input으로 사용
-
-이 때 reduce시 summation과 average 방법을 둘 다 사용해보고, 이를 비교
